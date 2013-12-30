cf19eddc8c1013d73a3c7a8dd2b85b7016aa43f7
Gustavo <gustavo.santos0106@gmail.com>
2013-12-30 16:13:12 -0200

    enable cpu unaligned access, undervolt 133mhz busfreq a bit, silence a compiling warning

--- arch/arm/mach-exynos/busfreq.c
+++ arch/arm/mach-exynos/busfreq.c
@@ -103,13 +103,13 @@ struct busfreq_table {
 
 static struct busfreq_table exynos4_busfreq_table[] = {
 	{LV_0, 400000, 1100000, 0, 0},
-	{LV_1, 267000, 1000000, 0, 0},
+	{LV_1, 267000, 950000, 0, 0},
 #ifdef CONFIG_BUSFREQ_L2_160M
 	/*L2: 160MHz */
-	{LV_2, 160000, 1000000, 0, 0},
+	{LV_2, 160000, 900000, 0, 0},
 #else
 	/* L2: 133MHz */
-	{LV_2, 133000, 950000, 0, 0},
+	{LV_2, 133000, 900000, 0, 0},
 #endif
 	{0, 0, 0, 0, 0},
 };
@@ -173,7 +173,7 @@ static unsigned int exynos4_asv_volt[ASV_GROUP][LV_END] = {
 	{1125000, 1025000, 1025000},
 	{1100000, 1000000, 1000000},
 	{1075000, 975000, 975000},
-	{1050000, 950000, 950000},
+	{1050000, 950000, 900000},
 };
 
 static unsigned int clkdiv_dmc0[LV_END][8] = {
