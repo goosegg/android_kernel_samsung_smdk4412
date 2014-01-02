27925926d1548c6419f1e6775323fcd8b88bf941
Gustavo <gustavo.santos0106@gmail.com>
2014-01-02 17:23:17 -0200

    cma: Add API to get the start address of a CMA region When setting CMA up at a fixed region, it is possible for the address to be shared between multiple subsystems. If the address is placed dynamically, there is no mechanism to be able to get the start address of the region. Drivers may wish to do keep track of allocations relative to the start address so add an API to get the start region of a CMA address.
    Change-Id: If0730c64496c876d3143064d767b22b984c6dc84
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -221,6 +221,13 @@ static int __init cma_init_reserved_areas(void)
 }
 core_initcall(cma_init_reserved_areas);
 
+phys_addr_t cma_get_base(struct device *dev)
+{
+  struct cma *cma = dev_get_cma_area(dev);
+
+  return cma->base_pfn << PAGE_SHIFT;
+}
+
 /**
  * dma_declare_contiguous() - reserve area for contiguous memory handling
  *			      for particular device
